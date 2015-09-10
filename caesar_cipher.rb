09e79d820c1da8c9f51a436fe018cce61bf9966a
nabeelh92 <nabeelhuq@Nabeels-MacBook-Pro.local>
2015-09-09 20:57:00 -0700

    added implementation

---
+++ caesar_cipher.rb
@@ -0,0 +1,22 @@
+# bad time complexity but it works
+def cipher(word, n)
+	new_word = ""
+	word.each_char do |i|
+		n.times do
+			if(i == "z")
+				i = "a"
+				next
+			elsif(i == "Z")
+				i = "A"
+				next
+			end
+			i.next!
+			i == "%" ? i = " " : ""
+		end
+		new_word += i	
+	end
+	puts new_word
+end
+
+cipher("What a string!", 5)
+cipher("hello", 9)
