afcda79a661e1d855da07c9d62b1616393209d53
nabeelh92 <nabeelhuq@Nabeels-MacBook-Pro.local>
2015-09-19 15:17:55 -0700

    added bubble sort method

---
+++ bubble_sort.rb
@@ -0,0 +1,25 @@
+def bubble_sort(arr)
+	n = arr.length
+
+	loop do
+		is_swapped = false
+
+		(n-1).times do |i|
+			if arr[i] > arr[i+1]
+				arr[i], arr[i+1] = arr[i+1], arr[i]
+				is_swapped = true
+			end 
+		end
+
+		break if not is_swapped
+	end
+
+	arr
+end
+
+
+my_array = [4,3,78,2,0,2]
+puts "BEFORE bubble_sort is called, the array is #{my_array}"
+
+bubble_sort(my_array)
+puts "AFTER bubble_sort is called, the array is #{my_array}"
