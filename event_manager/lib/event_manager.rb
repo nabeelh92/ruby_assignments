9d7a0bcfdaa75b64e8dbae42970bbfecb7da153b
nabeelh92 <nabeelhuq@Nabeels-MacBook-Pro.local>
2015-10-05 14:23:58 -0700

    added .erb forms

--- event_manager/lib/event_manager.rb
+++ event_manager/lib/event_manager.rb
@@ -1,5 +1,6 @@
 require "csv"
 require "sunlight/congress"
+require "erb"
 
 Sunlight::Congress.api_key = "e179a6973728c4dd3fb1204283aaccb5"
 
@@ -9,41 +10,41 @@ end
 
 def legislators_by_zipcode(zipcode)
 	legislators = Sunlight::Congress::Legislator.by_zipcode(zipcode)
-
-	legislator_names = legislators.collect do |legislator|
-    	"#{legislator.first_name} #{legislator.last_name}"
-  	end
-
-  	legislator_string = legislator_names.join(", ")
 end
 
 
 puts "Event manager initialized."
 
-=begin
-def clean_zipcode(zipcode)
-	if zipcode.nil?
-		"00000"
-	elsif zipcode.length < 5
-		zipcode = zipcode.rjust(5,"0")
-	elsif zipcode.length > 5
-		zipcode = zipcode[0..4]
-	else
-		zipcode
-	end
-end
-=end
-
-
 
 contents = CSV.open "event_attendees.csv", headers: true, header_converters: :symbol
 
+template_letter = File.read "form_letter.erb"
+erb_template = ERB.new template_letter
 
 contents.each do |row|
 	name = row[:first_name]
+	
 	zipcode = clean_zipcode( row[:zipcode] )
 
 	legislators = legislators_by_zipcode(zipcode)
+
+	form_letter = erb_template.result(binding)
 	
-	puts "#{name} #{zipcode} #{legislators}"
+	puts form_letter
+	#puts "#{name} #{zipcode} #{legislators}"
 end
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
