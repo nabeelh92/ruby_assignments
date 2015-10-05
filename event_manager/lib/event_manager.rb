eb5fee6c0a2635c97fb805019d5cafb3d93f1d1b
nabeelh92 <nabeelhuq@Nabeels-MacBook-Pro.local>
2015-10-05 13:47:03 -0700

    added sunlight_legislator AP calls

--- event_manager/lib/event_manager.rb
+++ event_manager/lib/event_manager.rb
@@ -1,12 +1,49 @@
+require "csv"
+require "sunlight/congress"
+
+Sunlight::Congress.api_key = "e179a6973728c4dd3fb1204283aaccb5"
+
+def clean_zipcode(zipcode)
+	zipcode.to_s.rjust(5,"0")[0..4]
+end
+
+def legislators_by_zipcode(zipcode)
+	legislators = Sunlight::Congress::Legislator.by_zipcode(zipcode)
+
+	legislator_names = legislators.collect do |legislator|
+    	"#{legislator.first_name} #{legislator.last_name}"
+  	end
+
+  	legislator_string = legislator_names.join(", ")
+end
+
+
 puts "Event manager initialized."
 
-#contents = File.read "event_attendees.csv"
-#puts contents
+=begin
+def clean_zipcode(zipcode)
+	if zipcode.nil?
+		"00000"
+	elsif zipcode.length < 5
+		zipcode = zipcode.rjust(5,"0")
+	elsif zipcode.length > 5
+		zipcode = zipcode[0..4]
+	else
+		zipcode
+	end
+end
+=end
 
-lines = File.readlines "event_attendees.csv"
-lines.each do |line|
-	next if line == " ,RegDate,first_Name,last_Name,Email_Address,HomePhone,Street,City,State,Zipcode\n"
-	columns = line.split(",")
-	name = columns[2]
-	puts name
+
+
+contents = CSV.open "event_attendees.csv", headers: true, header_converters: :symbol
+
+
+contents.each do |row|
+	name = row[:first_name]
+	zipcode = clean_zipcode( row[:zipcode] )
+
+	legislators = legislators_by_zipcode(zipcode)
+	
+	puts "#{name} #{zipcode} #{legislators}"
 end
