fa20b1945e31799a65d2c45e29a8df13f4983a41
nabeelh92 <nabeelhuq@Nabeels-MacBook-Pro.local>
2015-10-05 17:18:36 -0700

    adding twitter bot basic implementation

--- twitter_bot/micro_blogger.rb
+++ twitter_bot/micro_blogger.rb
@@ -1,9 +1,25 @@
 require 'jumpstart_auth'
 
-class Microblogger
+class MicroBlogger
 	attr_reader :client
 
 	def initialize
-		puts "initializing Microblogger"
+		puts "initializing Microblogger...."
+		@client = JumpstartAuth.twitter
+	end
+
+	def tweet(message)
+	  if(message.length > 140)
+	    puts "ERROR, you message has to be 140 characters or less"
+	  else	
+		@client.update(message)
+	  end
+	end
+
+	def run
+		puts "Welcome to the JSL Twitter Client!"
 	end
 end
+
+blogger = MicroBlogger.new
+blogger.run
